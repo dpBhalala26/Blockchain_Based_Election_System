f3a567eef84edd11436ba98ea7c73702e9a17ab1
Devang Bhalala <bhalaladevang@gmail.com>
2021-02-18 00:43:21 +0530

    Implemented constuctor for smart-contract service.

--- SecurElect/src/app/core/blockchain/smart-contract.service.ts
+++ SecurElect/src/app/core/blockchain/smart-contract.service.ts
@@ -1,52 +1,73 @@
 import { Injectable } from '@angular/core';
+import { Web3Service } from './web3.service';
+
+declare let require: any;
+const electionArtifacts = require('../../../../build/contracts/ElectionContract.json');
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SmartContractService {
-
-  constructor() { }
+  electionContract: any;
+  constructor(private web3Service: Web3Service) {
+    console.log(
+      'In smart-contract service constructor: Web3Service is=' + web3Service
+    );
+    console.log('OnInit: ' + this.web3Service);
+    console.log(this);
+    // watch current account
+    this.web3Service
+      .convertArtifactsToContract(electionArtifacts)
+      .then((absContract) => {
+        this.electionContract = absContract;
+        this.electionContract.deployed().then((deployed) => {
+          console.log(
+            'OnInit: election contract deployed successfully.' + deployed
+          );
+        });
+      });
+  }
 
   // Function to initialize and migrate smart contract to the blockchain.
   // We have assumed that smart-contract will be migrated manually using truffle by the admin.
-  async migrateElectionContract(electionId: string, mnemonic: string, port: string, host:string){
+  async migrateElectionContract(
+    electionId: string,
+    mnemonic: string,
+    port: string,
+    host: string
+  ) {
     // API should accept election_id and return id, (candidateIds - name), (voterIds - public addresses), mnemonic, port, host
     // server api call to change state and description of election => uninitiazed to beforestart (string)
     // return string  ("Success") or throwError
   }
 
   // Function to add candidate for the election
-  private addCandidatesForElection(){
-
-  }
+  private addCandidatesForElection() {}
 
   // Function to make voters eligible for the election
-  private makeVotersEligibleForElection(){
-
-  }
+  private makeVotersEligibleForElection() {}
 
   // To initialize the voting process.
-  async initializeVotingProcess(electionId: string){
+  async initializeVotingProcess(electionId: string) {
     // server api call to change state of election => beforestart to started (string)
     // return string  ("Success") or throwError
   }
 
   // To cast vote
-  async castVote(candidateId: string){
+  async castVote(candidateId: string) {
     // return string ("Success") or throwError
   }
 
   // To finalize the voting process
-  async finalizeVotingProcess(electionId: string){
+  async finalizeVotingProcess(electionId: string) {
     // return string  ("Success") or throwError
   }
 
-  async getCandidateVoteCounts(electionId: string){
+  async getCandidateVoteCounts(electionId: string) {
     // returns Candidate[]
   }
 
-  async getWinningCandidates(electionId: string){
+  async getWinningCandidates(electionId: string) {
     // returns Candidate[]
   }
-
 }
