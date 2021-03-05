7835620bad265a3019d63e766891eca4e4433e57
Devang Bhalala <bhalaladevang@outlook.com>
2021-03-05 21:53:29 +0530

    Created Home Page for SecurElect.

--- SecurElect/src/app/election-admin/election-admin-routing.module.ts
+++ SecurElect/src/app/election-admin/election-admin-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ElectionDetailsComponent } from '../shared/election-details/election-details.component';
 import { ElectionsComponent } from './elections/elections.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './../home/home.component';
 import { ManageVotersComponent } from './manage-voters/manage-voters.component';
 import { SetElectionComponent } from './set-election/set-election.component';
 import { ViewElectionComponent } from './view-election/view-election.component';
--- SecurElect/src/app/voter/voter-routing.module.ts
+++ SecurElect/src/app/voter/voter-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CastVoteComponent } from './cast-vote/cast-vote.component';
 import { ElectionsJoinedComponent } from './elections-joined/elections-joined.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './../home/home.component';
 import { ViewElectionJoinedComponent } from './view-election-joined/view-election-joined.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     pathMatch: 'full',
     component: HomeComponent,
   },
